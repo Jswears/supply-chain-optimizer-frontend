ded615af30b717ee1ef80342d9fdb51c41f2ce42
Jswears <ji.swearssalinas@gmail.com>
2025-04-03 14:04:03 +0200

    feat(product-details): adding dynamic metadata to proudct details

--- src/app/dashboard/products/[warehouseId]/[productId]/page.tsx
+++ src/app/dashboard/products/[warehouseId]/[productId]/page.tsx
@@ -1,10 +1,28 @@
 import { DashboardLayout } from "@/components/dashboard/dashboard-layout";
 import ProductDetail from "@/components/products/product-detail";
-import { Metadata } from "next";
+import { useProductsStore } from "@/stores/productsStore";
+import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-    title: "Product Details | ChainOPT",
-    description: "Product details and inventory information",
+
+const fetchProductById = useProductsStore.getState().fetchProductById;
+
+type Props = {
+    params: Promise<{ productId: string, warehouseId: string }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateMetadata({
+    params }: Props,
+): Promise<Metadata> {
+    const { productId, warehouseId } = await params;
+
+    const product = await fetchProductById(productId, warehouseId);
+    const title = product?.product_name ?? "Product";
+    const description = `Product details for ${title}`;
+    return {
+        title: `${title} | ChainOPT Inventory Dashboard`,
+        description: description,
+    }
 }
 
 const ProductPage = async ({ params }: {
