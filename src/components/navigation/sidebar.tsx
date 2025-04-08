9e97725d61918bf8474500a01e590b23d5d64d52
Joaquin Swears Salinas <106037859+Jswears@users.noreply.github.com>
2025-04-08 10:10:16 +0200

    fix(sidebar): fixing the sidebar refresh (#12)

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -8,11 +8,13 @@ import {
     SidebarMenu,
     SidebarMenuItem,
     SidebarMenuButton,
+    SidebarRail,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 import { BarChart3, User, BoxIcon, ShoppingBag, LogOut } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { useAuthStore } from "@/stores/authStore";
+import Image from "next/image";
 
 const SidebarComponent = () => {
     const pathname = usePathname();
@@ -43,14 +45,22 @@ const SidebarComponent = () => {
     };
 
     return (
-        <Sidebar>
+        <Sidebar collapsible="icon">
             <SidebarHeader className="flex items-center justify-center py-6">
-                <h1 className="text-xl font-bold text-white">Bakery Dashboard</h1>
+                <div className="flex items-center space-x-2">
+                    <Image src={"/logo.png"} alt="logo" width={32} height={32} />
+                    <h1 className="text-xl font-bold text-white group-data-[collapsible=icon]:hidden ">ChainOpt AI</h1>
+
+                </div>
             </SidebarHeader>
             <SidebarContent>
                 <SidebarMenu>
                     <SidebarMenuItem>
-                        <SidebarMenuButton asChild isActive={isActive("/")}>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={isActive("/")}
+                            tooltip="Products"
+                        >
                             <Link href="/dashboard/products">
                                 <BoxIcon />
                                 <span>Products</span>
@@ -58,7 +68,11 @@ const SidebarComponent = () => {
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                     <SidebarMenuItem>
-                        <SidebarMenuButton asChild isActive={isActive("/dashboard/orders")}>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={isActive("/dashboard/orders")}
+                            tooltip="Orders"
+                        >
                             <Link href="/dashboard/orders">
                                 <ShoppingBag />
                                 <span>Orders</span>
@@ -66,7 +80,11 @@ const SidebarComponent = () => {
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                     <SidebarMenuItem>
-                        <SidebarMenuButton asChild isActive={isActive("/forecasts")}>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={isActive("/forecasts")}
+                            tooltip="Forecasts"
+                        >
                             <Link href="/dashboard/forecasts">
                                 <BarChart3 />
                                 <span>Forecasts</span>
@@ -78,7 +96,11 @@ const SidebarComponent = () => {
             <SidebarFooter>
                 <SidebarMenu>
                     <SidebarMenuItem>
-                        <SidebarMenuButton asChild isActive={isActive("/dashboard/profile")}>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={isActive("/dashboard/profile")}
+                            tooltip="Profile"
+                        >
                             <Link href="/dashboard/profile">
                                 <User />
                                 <span>Profile</span>
@@ -86,13 +108,18 @@ const SidebarComponent = () => {
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                     <SidebarMenuItem>
-                        <SidebarMenuButton className="cursor-pointer" onClick={handleLogout}>
+                        <SidebarMenuButton
+                            className="cursor-pointer"
+                            onClick={handleLogout}
+                            tooltip="Logout"
+                        >
                             <LogOut />
                             <span>Logout</span>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarFooter>
+            <SidebarRail />
         </Sidebar>
     );
 };
